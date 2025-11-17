7645a9cc53afe29e4e9d45f0ac4e7d387d369a53
Ian Light <86308592+ilight1542@users.noreply.github.com>
2025-11-17 15:19:55 +0100

    attempted refactoring of unified snakemakeslurm.sh

--- snakemakeslurm.sh
+++ snakemakeslurm.sh
@@ -1,4 +1,90 @@
-#!/bin/bash
+#! /bin/bash 
+## defaults, will be overwritten if redefined in options parsing
+script_name=$(basename $0)
+prog_version=0.1.0
+
+# default behavior: run all steps:
+onlycmt=false
+onlyposteager=false
+
+## functions
+
+# Function to display script usage
+usage() {
+    echo "Usage: $0 -I <file> -O <file> [options]"
+    echo "Options:"
+    echo "  -c, --cmt           Run only cmt snakemake (case)"
+    echo "  -p, --posteager     Run only post eager snakemake (mapping)"
+    echo "  -v, --version       Show version"
+    echo "  -h, --help          Show help"
+    exit 1
+}
+# Parse command-line options
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -c|--cmt)
+            onlycmt=true
+            shift
+            ;;
+        -p|--posteager)
+            onlyposteager=true
+            shift
+            ;;
+        -v|--version)
+            version=true
+            shift
+            ;;
+        -h|--help)
+            help=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $key"
+            usage
+            ;;
+    esac
+done
+
+help() { # print help, explanation for all parameters
+    printf  "
+    $(basename ${script_name^^})
+    $script_name - Initialize unified ancient snakemake (post eager --> CMT generation)
+    
+    OPTIONS
+        Options:
+        -p
+            --posteager - Only run the posteager.Snakefile. Only without also invoking -c/--cmt
+        -c
+            --cmt - Only run the cmt.Snakefile, if restarting a run that failed on this section. Only without also invoking -p/--posteager
+        -h     
+            --help - Print this help message
+        -v      
+            --version - Print version
+
+    AUTHOR
+        Ian Light-Maka (ilight1542@gmail.com)
+
+    VERSION
+        ${prog_version}
+    \n    
+    "
+}
+
+## validation ##
+if [[ $version == true ]]; then
+  printf $prog_version
+  exit 0
+fi
+
+if [[ $help == true ]]; then help && exit 0; fi
+
+if [[ $onlycmt == true && $onlyposteager == true  ]]
+    then
+    echo "ERROR: Only -c OR -p can be set at once"
+    usage
+fi
+
 
 # launch snakemake to run jobs via SLURM
 # ntasks
@@ -41,9 +127,9 @@ then
     echo "Changed 'scripts/slurm_status.py' to executable";
 fi
 
-snakemake -p \
-    --snakefile posteager.Snakefile \
-    $* \
+call_snakemake() {
+    snakemake -p \
+    --snakefile $1 \
     --latency-wait 60 \
     -j 100 \
     --cluster-config $(dirname $0)/cluster.slurm.json \
@@ -55,26 +141,21 @@ snakemake -p \
     --keep-going \
     --use-conda \
     --conda-prefix /nexus/posix0/MPIIB-keylab/snakemake_conda_envs/ \
-    --group-components make_link_group=100000 \
-&& \
-snakemake -p \
-    --snakefile cmt.Snakefile \
-    $* \
-    --latency-wait 60 \
-    -j 100 \
-    --cluster-config $(dirname $0)/cluster.slurm.json \
-    --cluster "sbatch ${SM_ARGS}" \
-    --cluster-status scripts/slurm_status_script.py \
-    --default-resources "tmpdir='/ptmp/${USER}/tmp'" \
-    --group-components var2pos=200 \
-    --rerun-incomplete \
-    --restart-times 1 \
-    --keep-going \
-    --use-conda \
-    --conda-prefix /nexus/posix0/MPIIB-keylab/snakemake_conda_envs/
+    --group-components make_link_group=100000 var2pos=200 
+}
 
+if [[ $onlyposteager == true ]]
+then
+    call_snakemake posteager.Snakefile 
+    exit 0
+fi
 
-    #--dry-run \
-    #--group-components \
-    # --dag \
-    # | dot -Tsvg > dag.svg
+if [[ $onlycmt == true ]]
+then
+    call_snakemake cmt.Snakefile 
+    exit 0
+fi
+
+# unified call (default behavior)
+call_snakemake posteager.Snakefile && call_snakemake cmt.Snakefile
+exit 0
